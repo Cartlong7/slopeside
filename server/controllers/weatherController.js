b157b0948c34f5de0965379ddad9121db63b62e0
Cartlong7 <crlong7@gmail.com>
2023-04-05 14:24:26 -0600

    able to see resort object and url in console. Close to working backend

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -12,27 +12,6 @@ React Client -> Express Server -> weather.gov API -> Express Server -> MongoDB D
 */
 
 // Controller will be the middleware that will provide the frontend weather data through an api call
-
-// weatherController.getWeather = (req, res, next) => {
-
-//   const { latitude, longitude } = req.query;
-
-//   try {
-//     // const response = axios.get(`https://api.weather.gov/points/${latitude},${longitude}/forecast`);
-//     // const response = axios.get(`https://api.weather.gov/points/${resorts.name.latitude},${resorts.name.longitude}/forecast`);
-//     console.log(req.query);
-//     const response = axios.get('https://api.weather.gov/gridpoints/BOU/22,54/forecast');
-//     const resort = Weather.findOneandUpdate({ latitude, longitude }, { weatherData: response.data }, { upsert: true, new: true });
-//     res.locals.resort = resort;
-//     return next();
-//   }
-//   catch (err) {
-//     return next(err);
-//   }
-// };
-
-// module.exports = weatherController;
-
   
 /* functionality here:
   fetch weather data from api based on the key used on the front end
@@ -44,8 +23,6 @@ React Client -> Express Server -> weather.gov API -> Express Server -> MongoDB D
   - where the logic goes is the challenge
   */
  
- 
-// api call to weather api using latitude and longitude
 
 weatherController.getWeather = async (req, res, next) => {
   const { latitude, longitude } = req.query;
@@ -54,18 +31,21 @@ weatherController.getWeather = async (req, res, next) => {
     const resort = resorts.find(
       (r) => r.latitude === latitude && r.longitude === longitude
     );
+    console.log(resort);
+
     if (!resort) {
       throw new Error('Resort not found');
     }
 
     const url = `https://api.weather.gov/gridpoints/${resort.gridOffice}/${resort.gridX},${resort.gridY}/forecast`;
-    
+
+    console.log(url);
+
     const response = await axios.get(url);
 
     const weatherData = {
       temperature: response.data.properties.periods[0].temperature,
-      probabilityOfPrecipitation:
-        response.data.properties.periods[0].probabilityOfPrecipitation,
+      probabilityOfPrecipitation: response.data.properties.periods[0].probabilityOfPrecipitation.value,
       windSpeed: response.data.properties.periods[0].windSpeed,
       icon: response.data.properties.periods[0].icon,
       shortForecast: response.data.properties.periods[0].shortForecast,
@@ -77,6 +57,8 @@ weatherController.getWeather = async (req, res, next) => {
       { weatherData },
       { upsert: true, new: true }
     );
+    
+    console.log(updatedWeather);
 
     res.locals.resort = updatedWeather;
     return next();
